7e184facbfe2a1abfe792313986d414b6d99378d
Samual Norman <me@samual.uk>
2026-01-07 15:47:30 +0000

    remove `@samual/lib` by reimplementing `getExports()` without it

--- scripts/lib/exports.js
+++ scripts/lib/exports.js
@@ -1,16 +1,27 @@
-import { findFiles } from "@samual/lib/findFiles"
+import { readdir as readFolder } from "fs/promises"
+import Path from "path"
+
+/**
+ * @arg {import("fs").Dirent} dirent
+ * @return {string}
+ */
+const getDirentParentPath = dirent => /** @type {any} */ (dirent).parentPath ?? dirent.path
 
 export const getExports = async (
 	/** @type {string} */ queryFileExtension,
 	/** @type {string | undefined} */ outputFileExtension = queryFileExtension
-) => ({
+) => /** @type {Record<string, string>} */ ({
 	".": `./default${outputFileExtension}`,
-	...Object.fromEntries((await findFiles(`dist`))
-		.filter(path => path != `dist/default${queryFileExtension}` && path.endsWith(queryFileExtension))
-		.map(path => {
-			const sliced = `.${path.slice(4, -queryFileExtension.length)}`
+	...Object.fromEntries(
+		(await readFolder(`dist`, { withFileTypes: true, recursive: true }))
+			.filter(dirent => dirent.isFile())
+			.map(dirent => Path.join(getDirentParentPath(dirent), dirent.name))
+			.filter(path => path != `dist/default${queryFileExtension}` && path != `dist/internal${queryFileExtension}` && path.endsWith(queryFileExtension))
+			.sort()
+			.map(path => {
+				const sliced = `.${path.slice(4, -queryFileExtension.length)}`
 
-			return [ sliced, sliced + outputFileExtension ]
-		})
+				return [ sliced, sliced + outputFileExtension ]
+			})
 	)
 })
