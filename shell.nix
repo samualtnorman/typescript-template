faccb00401f90cb462e46b37c83995a2107fb843
Samual Norman <me@samual.uk>
2026-03-29 21:21:37 +0100

    update `shell.nix`

--- shell.nix
+++ shell.nix
@@ -4,8 +4,8 @@ let fetchNixpkgs =
 	{ rev, sha256 ? "" }: import (fetchFromGitHub { owner = "NixOS"; repo = "nixpkgs"; inherit rev sha256; }) {}; in
 
 let inherit (fetchNixpkgs {
-	rev = "5912c1772a44e31bf1c63c0390b90501e5026886"; # unstable 2026/01/08
-	sha256 = "Mj3d3PfwltLmukFal5i3fFt27L6NiKXdBezC1EBuZs4=";
+	rev = "1073dad219cb244572b74da2b20c7fe39cb3fa9e"; # unstable 2026/03/29
+	sha256 = "tFwzTI0DdDzovdE9+Ras6CUss0yn8P9XV4Ja6RjA+nU=";
 }) nodejs_24 pnpm_10; in
 
 mkShellNoCC {
@@ -13,8 +13,8 @@ mkShellNoCC {
 		cacert git nodejs_24
 		(pnpm_10.override {
 			nodejs = nodejs_24;
-			version = "10.27.0";
-			hash = "sha256-08fD0S2H0XfjLwF0jVmU+yDNW+zxFnDuYFMMN0/+q7M=";
+			version = "10.33.0";
+			hash = "sha256-v8wby60nmxOlFsRGp1s8WLaQS0XVehlRQRAV5Qt1GoA=";
 		})
 	];
 }
